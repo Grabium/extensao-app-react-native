e0b643dfd2da992197d0c5d33faa21f430323524
Gabriel <110872805+Grabium@users.noreply.github.com.>
2025-01-20 23:31:16 -0300

    #2-Instalações para imlementar Stack: yarn add @react-navigation/native yarn add react-native-screens react-native-safe-area-context yarn add @react-navigation/native-stack
    Alteração em MainActivity.kt (kotlin)

--- index.js
+++ index.js
@@ -3,7 +3,23 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+//import App from './App';
+import App from './src/app';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+
+
+
+/*
+Instalados:
+
+yarn add axios
+yarn add @react-navigation/native
+yarn add react-native-screens react-native-safe-area-context
+yarn add @react-navigation/native-stack
+
+Alteração em MainActivity.kt (kotlin)
+
+
+*/
